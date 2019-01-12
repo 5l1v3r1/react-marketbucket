9ec91b87f21d1691fa14ed8ab4d5d6dff4e61115
Ahmed Ramzi <ahmedramzy160@yahoo.com>
2019-01-13 01:55:20 +0800

    added signup page with friendly UI for error handling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import SignUp from './pages/SignUp';
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Fragment>
+        <Switch>
+          <Route path="/signup"
+            component={props =>
+              <SignUp {...props} />
+            }>
+          </Route>
+          {/* <Route path="/login"
+            component={props =>
+              <Login {...props} />
+            }>
+          </Route> */}
+        </Switch>
+      </Fragment>
+    )
   }
 }
-
 export default App;
